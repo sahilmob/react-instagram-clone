aac99abd389bfd440a9e2771a02d98a944b53d4c
Sahil Mobaidin <sahil.hmob@hotmail.com>
2018-09-09 16:10:39 +0300

    Add FooterMenu component

---
+++ src/components/FooterMenu/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default () => {
+    return (
+        <div>
+            <ul>
+                <li>about us</li>
+                <li>support</li>
+                <li>press</li>
+                <li>api</li>
+                <li>jobs</li>
+                <li>privacy</li>
+                <li>terms</li>
+                <li>profile</li>
+                <li>hashtags</li>
+                <li>language</li>
+            </ul>
+        </div>
+    )
+}
--- src/containers/LandingPage/index.jsx
+++ src/containers/LandingPage/index.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import FooterMenu from '../../components/FooterMenu/'
 
 export default class LandingPage extends Component {
     render() {
         return (
             <div>
                 Landing Page
+                <FooterMenu />
             </div>
         )
     }
