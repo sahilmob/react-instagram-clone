1a3d8970c577f1d0c9ffd53dbd7aa764f2f916a7
Sahil Mobaidin <sahil.hmob@hotmail.com>
2018-09-09 17:54:47 +0300

    Add basic structure for landing page

--- src/containers/LandingPage/index.jsx
+++ src/containers/LandingPage/index.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
+import phoneImg from '../../statics/43cc71bb1b43.png'
 
 export default class LandingPage extends Component {
     render() {
         return (
-            <div>
-                Landing Page
+            <div className="container">
+                <div className="left">
+                    <img src={phoneImg} alt="" />
+                </div>
+                <div className="right">
+                    right
+                </div>
             </div>
         )
     }
