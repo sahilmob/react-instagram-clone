01d79a78edefa37dcbd078907a6cd74ec241bb8f
Sahil Mobaidin <sahil.hmob@hotmail.com>
2018-09-09 17:38:16 +0300

    Move FootereMenu to App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import LandingPage from './containers/LandingPage/'
+import FooterMenu from './components/FooterMenu/'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <Switch>
-          <Route path="/" exact component={LandingPage} />
-          <Route path="/login" exact render={() => <div> Login </div>} />
-          <Route path="/register" exact render={() => <div> Register </div>} />
-        </Switch>
-      </BrowserRouter>
+      <div>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/" exact component={LandingPage} />
+            <Route path="/login" exact render={() => <div> Login </div>} />
+            <Route path="/register" exact render={() => <div> Register </div>} />
+          </Switch>
+        </BrowserRouter>
+        <FooterMenu />
+      </div>
     );
   }
 }
--- src/containers/LandingPage/index.jsx
+++ src/containers/LandingPage/index.jsx
@@ -1,12 +1,10 @@
 import React, { Component } from 'react'
-import FooterMenu from '../../components/FooterMenu/'
 
 export default class LandingPage extends Component {
     render() {
         return (
             <div>
                 Landing Page
-                <FooterMenu />
             </div>
         )
     }
