adb9bddc76d756ea058518e1f14425126994e989
Sahil Mobaidin <sahil.hmob@hotmail.com>
2018-09-09 16:07:16 +0300

    Add landing page container

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import LandingPage from './containers/LandingPage/'
 import './App.css';
 
 class App extends Component {
@@ -7,7 +8,7 @@ class App extends Component {
     return (
       <BrowserRouter>
         <Switch>
-          <Route path="/" exact render={() => <div> Index </div>} />
+          <Route path="/" exact component={LandingPage} />
           <Route path="/login" exact render={() => <div> Login </div>} />
           <Route path="/register" exact render={() => <div> Register </div>} />
         </Switch>
