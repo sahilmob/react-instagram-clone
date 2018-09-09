5be131c9c37719bb8f88a8a9bebd4497e1065eda
Sahil Mobaidin <sahil.hmob@hotmail.com>
2018-09-09 15:34:10 +0300

    Setup router

--- src/App.js
+++ src/App.js
@@ -5,9 +5,13 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello World</h1>
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact render={() => <div> Index </div>} />
+          <Route path="/login" exact render={() => <div> Login </div>} />
+          <Route path="/register" exact render={() => <div> Register </div>} />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
