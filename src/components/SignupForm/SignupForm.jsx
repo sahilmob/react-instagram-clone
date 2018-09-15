f4eb7654eb3d3f595a225791e5170706ae60c264
Sahil Mobaidin <sahil.hmob@hotmail.com>
2018-09-15 21:13:34 +0300

    Setup two way binding between SignupForm state and Input value

--- src/components/SignupForm/SignupForm.jsx
+++ src/components/SignupForm/SignupForm.jsx
@@ -4,15 +4,27 @@ import classes from './SignupForm.css'
 import igLogo from '../../static/img/instagram.svg'
 
 export default class SignupForm extends Component {
+    state = {
+        number_or_email: '',
+        name: '',
+        username: '',
+        password: ''
+    }
     changeHandler = (event) => {
         var label = event.target.previousSibling
 
         if (event.target.value === '') {
             label.style.fontSize = "12px";
             label.style.top = "16px";
+            this.setState({
+                [event.target.name]: event.target.value
+            })
         } else {
             label.style.fontSize = "10px";
             label.style.top = "6px";
+            this.setState({
+                [event.target.name]: event.target.value
+            })
         }
     }
 
@@ -25,10 +37,10 @@ export default class SignupForm extends Component {
                 <h2 className={classes.headLine}>Sign up to see photos and videos from your friends.</h2>
                 <button className={fbBtnClasses.join(" ")}>Log in with Facebook</button>
                 <span className={classes.or}>or</span>
-                <Input inputType="text" labelText="Mobile Number or Email" nameText="number_or_email" changed={this.changeHandler} id="number_or_email" blur={this.blurHandler} />
-                <Input inputType="text" labelText="Full Name" nameText="name" changed={this.changeHandler} id="name" blur={this.blurHandler} />
-                <Input inputType="text" labelText="Username" nameText="username" changed={this.changeHandler} id="username" blur={this.blurHandler} />
-                <Input inputType="password" labelText="Password" nameText="password" changed={this.changeHandler} id="password" />
+                <Input inputValue={this.state.number_or_email} inputType="text" labelText="Mobile Number or Email" nameText="number_or_email" changed={this.changeHandler} id="number_or_email" blur={this.blurHandler} />
+                <Input inputValue={this.state.name} inputType="text" labelText="Full Name" nameText="name" changed={this.changeHandler} id="name" blur={this.blurHandler} />
+                <Input inputValue={this.state.username} inputType="text" labelText="Username" nameText="username" changed={this.changeHandler} id="username" blur={this.blurHandler} />
+                <Input inputValue={this.state.password} inputType="password" labelText="Password" nameText="password" changed={this.changeHandler} id="password" />
                 <button className={signUpBtnClasses.join(" ")}>Sign up</button>
                 <span className={classes.agreeMsg}>By signing up, you agree to our <strong>Terms, Data Policy</strong> and <strong>Cookies Policy.</strong></span>
 
--- src/components/UI/Input/Input.jsx
+++ src/components/UI/Input/Input.jsx
@@ -5,7 +5,7 @@ export default (props) => {
     return (
         <div className={classes.container}>
             <label htmlFor={props.id}>{props.labelText}</label>
-            <input className={classes.input} type={props.inputType} name={props.nameText} onChange={props.changed} id={props.id}/>
+            <input className={classes.input} type={props.inputType} name={props.nameText} onChange={props.changed} id={props.id} value={props.inputValue}/>
         </div>
     )
 }
