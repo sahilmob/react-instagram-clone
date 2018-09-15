902c79ff589cbe4d2a6cfc633b18f8cc087cd9c3
Sahil Mobaidin <sahil.hmob@hotmail.com>
2018-09-15 19:50:55 +0300

    Style PhoneMockup component

--- src/containers/LandingPage/LandingPage.jsx
+++ src/containers/LandingPage/LandingPage.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import SignupForm from '../../components/SignupForm/SignupForm'
 import RouteChanger from '../../components/RouteChanger/RouteChanger'
 import MobileAppsLinks from '../../components/MobileAppsLinks/MobileAppsLinks'
+import PhoneMockup from '../../components/PhoneMockup/PhoneMockup'
 import classes from './LandingPage.css'
 
 export default class LandingPage extends Component {
@@ -9,6 +10,7 @@ export default class LandingPage extends Component {
         return (
             <div className={classes.container}>
                 <div className="left">
+                    <PhoneMockup />
                 </div>
                 <div className="right">
                     <SignupForm />
