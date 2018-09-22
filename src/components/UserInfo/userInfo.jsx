6e58d74eeeb531e2f897b0472118c09f1e53fca8
Sahil Mobaidin <sahil.hmob@hotmail.com>
2018-09-22 22:47:50 +0300

    Finish user info section in profile page

--- src/components/UserInfo/userInfo.jsx
+++ src/components/UserInfo/userInfo.jsx
@@ -11,8 +11,25 @@ export default () => {
       <div className={calsses.middle}>
       <div className={calsses.top}>
         <span className={calsses.username}>JhonDoe</span>
-        <button>Edit Profile</button>
-        <FontAwesomeIcon icon="cog"/>
+        <button className={calsses.editProfileBtn}>Edit Profile</button>
+        <FontAwesomeIcon className={calsses.settingIcon} icon="cog"/>
+      </div>
+      <div className={calsses.center}>
+        <div className={calsses.stat}>
+        <span className={calsses.number}>15</span>
+        <span className={calsses.text}>posts</span>
+        </div>
+        <div className={calsses.stat}>
+        <span className={calsses.number}>285</span>
+        <span className={calsses.text}>followers</span>
+        </div>
+        <div className={calsses.stat}>
+        <span className={calsses.number}>633</span>
+        <span className={calsses.text}>following</span>
+        </div>
+      </div>
+      <div className={calsses.bottom}>
+      Jhon Doe
       </div>
       </div>
     </div>
