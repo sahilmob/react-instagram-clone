49f4d38a43bd611a2900fa3278c4be28eed383e7
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2018-10-01 01:46:37 +0300

    Add axios to call api, and get images for posts component

--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -1,23 +1,40 @@
-import React, { Component } from 'react'
-import classes from './Posts.css'
-import PostItem from '../PostItem/PostItem'
-import img1 from '../../static/img/1.jpg'
-import img2 from '../../static/img/2.jpg'
-import img3 from '../../static/img/3.jpg'
-import img4 from '../../static/img/4.jpg'
-import img5 from '../../static/img/5.jpg'
-import img6 from '../../static/img/6.jpg'
+import React, { Component } from "react";
+import classes from "./Posts.css";
+import PostItem from "../PostItem/PostItem";
+// import img1 from '../../static/img/1.jpg'
+// import img2 from '../../static/img/2.jpg'
+// import img3 from '../../static/img/3.jpg'
+// import img4 from '../../static/img/4.jpg'
+// import img5 from '../../static/img/5.jpg'
+// import img6 from '../../static/img/6.jpg'
+import axios from "axios";
 export default class Posts extends Component {
-  render() {
-    return (
-      <div className={classes.container} {...this.props}>
-      <PostItem sourc={img1} likes="32" comments="15"/>
-      <PostItem sourc={img2} likes="32" comments="15"/>
-      <PostItem sourc={img3} likes="32" comments="15"/>
-      <PostItem sourc={img4} likes="32" comments="15"/>
-      <PostItem sourc={img5} likes="32" comments="15"/>
-      <PostItem sourc={img6} likes="32" comments="15"/>
-      </div>
-    )
-  }
+	state = {
+		images: []
+	};
+	componentDidMount() {
+		axios
+			.get("https://react-instagram-nodejs-api.herokuapp.com/images")
+			.then(res => {
+				this.setState({
+					images: res.data
+				});
+			});
+	}
+
+	renderPosts = () => {
+		const posts = [];
+		this.state.images.map((img, i) => {
+			posts.push(<PostItem key={i} source={img} likes="32" comments="15" />);
+		});
+		return posts;
+	};
+
+	render() {
+		return (
+			<div className={classes.container} {...this.props}>
+				{this.renderPosts()}
+			</div>
+		);
+	}
 }
